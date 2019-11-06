7001ad77f51cad3125a06f3662b88d6afce74940
Diego <dimamuno@espol.edu.ec>
2019-11-06 08:13:12 -0500

    Base del juego

---
+++ Practica2/Assets/Code/TetrisBlock.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    public float tickDeltaTime;
+    public float fastTickDeltaTime;
+    public float speedFactor;
+
+    private Transform trans;
+    private float accDeltaTime;
+
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        accDeltaTime = 0f;   
+    }
+
+    void Start()
+    {
+        trans = this.transform;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        {//X-axis movement
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                var newPos = trans.position;
+                newPos.x -= 1;
+                trans.position = newPos;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                var newPos = trans.position;
+                newPos.x += 1;
+                trans.position = newPos;
+
+            }
+        }
+
+        { //Y-axis movement
+            accDeltaTime += Time.deltaTime;
+            var actualMovementTick = tickDeltaTime;
+
+            if (Input.GetKey(KeyCode.LeftShift)|| Input.GetKey(KeyCode.RightShift))
+            {
+                actualMovementTick = fastTickDeltaTime;
+                //actualMovementTick = speedFactor * tickDeltaTime;
+            }
+            if (accDeltaTime > actualMovementTick)
+            {
+                {//Move Down
+
+                    var newPos = trans.position;
+                    newPos.y -= 1;
+                    trans.position = newPos;
+
+                }
+
+                accDeltaTime = 0;
+            }
+        }
+
+    }
+}
