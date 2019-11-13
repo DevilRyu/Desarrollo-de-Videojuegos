7883d1ce2084bea517f865d33c83a707845433ff
Diego <dimamuno@espol.edu.ec>
2019-11-13 08:26:54 -0500

    Rotación corregida y colisión entre bloques

--- Practica2/Assets/Code/TetrisBlock.cs
+++ Practica2/Assets/Code/TetrisBlock.cs
@@ -1,111 +1,140 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * To Do
+ * - Block instantiation
+ * - Blocks collision
+ * - Line clearance
+ * - Losing condition
+ */
 public class TetrisBlock : MonoBehaviour
 {
     public float tickDeltaTime;
     public float fastTickDeltaTime;
-    public float speedFactor;
     public TetrisBoard Board;
+    public bool CanInstantiate = true;
 
     private Transform trans;
     private float accDeltaTime;
 
-    // Start is called before the first frame update
-
     void Awake()
     {
-        accDeltaTime = 0f;   
+        accDeltaTime = 0f;
     }
 
+    // Start is called before the first frame update
     void Start()
     {
-        trans = this.transform;    
+        trans = this.transform;
     }
 
-    (bool,bool) ApplyConstraints(Vector3 rollbackPos, Quaternion rollbackRot)
-    {
-        var (isOutX,isOutY) = CheckConstraints();
-        if (isOutX|| isOutY)
-        {
-            trans.position = rollbackPos;
-            trans.rotation = rollbackRot;
-        }
-
-        return (isOutX,isOutY);
-
-    }
-
-    (bool,bool) CheckConstraints()
+    (bool, bool, bool) CheckConstraints()
     {
         var isOutX = false;
         var isOutY = false;
+        var isGridNotAvailable = false;
         for (int i = 0; i < trans.childCount; i++)
         {
             var childTrans = trans.GetChild(i);
-            if(childTrans.position.x <0 || childTrans.position.x > 11)
+            int x = Mathf.RoundToInt(childTrans.position.x);
+            int y = Mathf.RoundToInt(childTrans.position.y);
+            if (x < 0
+                || x > 9)
             {
                 isOutX = true;
                 break;
             }
-            if (childTrans.position.y < 0)
+            if (y < 0)
             {
                 isOutY = true;
                 break;
             }
+            var row = y;
+            var col = x;
+            if (!Board.IsCellAvailable(row, col))
+            {
+                isGridNotAvailable = true;
+                break;
+            }
         }
-        return (isOutX,isOutY);
+        return (isOutX, isOutY, isGridNotAvailable);
+    }
+
+    (bool, bool, bool) ApplyConstraints(Vector3 rollbackPos,
+                                        Quaternion rollbackRot)
+    {
+        var (isOutX, isOutY, isGridNotAvailable) = CheckConstraints();
+        if (isOutX
+            || isOutY
+            || isGridNotAvailable)
+        {
+            trans.position = rollbackPos;
+            trans.rotation = rollbackRot;
+        }
+        return (isOutX, isOutY, isGridNotAvailable);
     }
 
     // Update is called once per frame
     void Update()
     {
-        {//Rotation
+        { // Rotation
             if (Input.GetKeyDown(KeyCode.UpArrow))
             {
                 var oldPos = trans.position;
                 var oldRot = trans.rotation;
+
                 trans.rotation *= Quaternion.Euler(0, 0, 90);
-                ApplyConstraints(oldPos,oldRot);
-                //trans.Rotate(Vector3.forward,90);
-                //trans.Rotare(new Vector3(0,0,90),Space.Self);
+                //trans.Rotate(Vector3.forward, 90);
+                //trans.Rotate(new Vector3(0, 0, 90), Space.Self);
+
+                ApplyConstraints(oldPos, oldRot);
             }
         }
 
-        {//X-axis movement
+        { // X-axis movement
             if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
+            { // Move left
                 var oldPos = trans.position;
+                var oldRot = trans.rotation;
+
                 var newPos = trans.position;
                 newPos.x -= 1;
                 trans.position = newPos;
-                ApplyConstraints(oldPos,transform.rotation);
-                
+
+                ApplyConstraints(oldPos, oldRot);
             }
-            else if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            { // Move Right
                 var oldPos = trans.position;
+                var oldRot = trans.rotation;
+
                 var newPos = trans.position;
                 newPos.x += 1;
                 trans.position = newPos;
-                ApplyConstraints(oldPos,transform.rotation);
+
+                ApplyConstraints(oldPos, oldRot);
             }
         }
 
-        { //Y-axis movement
+        { // Y-axis movement
             accDeltaTime += Time.deltaTime;
-            var actualMovementTick = tickDeltaTime;
 
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                actualMovementTick = fastTickDeltaTime;
-                //actualMovementTick = speedFactor * tickDeltaTime;
-            }
+            //var actualMovementTick = tickDeltaTime;
+            //if (Input.GetKey(KeyCode.LeftShift)
+            //    || Input.GetKey(KeyCode.RightShift))
+            //{
+            //    actualMovementTick = fastTickDeltaTime;
+            //    //actualMovementTick = speedFactor * tickDeltaTime;
+            //}
+
+            var fastSpeedPressed = Input.GetKey(KeyCode.DownArrow);
+            var actualMovementTick = fastSpeedPressed ? fastTickDeltaTime : tickDeltaTime;
+
             if (accDeltaTime > actualMovementTick)
             {
-                {//Move Down
-
+                { // Move down
                     var oldPos = trans.position;
                     var oldRot = trans.rotation;
 
@@ -113,19 +142,25 @@ public class TetrisBlock : MonoBehaviour
                     newPos.y -= 1;
                     trans.position = newPos;
 
-                    var(isOutX,isOutY)=ApplyConstraints(oldPos,oldRot);
+                    var (isOutX, isOutY, isGridNotAvailable) = ApplyConstraints(oldPos, oldRot);
 
-                    var done = false;
+                    var done = isOutY || isGridNotAvailable;
                     if (done)
                     {
-                        this.enabled=false;
-                        Board.InstantiateTetrisBlock();
+                        this.enabled = false;
+                        //this.gameObject.SetActive(false);
+
+                        if (CanInstantiate)
+                        {
+                            Board.InstantiateTetrisBlock();
+                            CanInstantiate = false;
+                        }
+                        Board.AddBlockToGrid(this);
+                        Board.ApplyClearanceRule();
                     }
                 }
-
                 accDeltaTime = 0;
             }
         }
-
     }
 }
--- Practica2/Assets/Code/TetrisBoard.cs
+++ Practica2/Assets/Code/TetrisBoard.cs
@@ -4,24 +4,93 @@ using UnityEngine;
 
 public class TetrisBoard : MonoBehaviour
 {
-
     public Transform SpawnPoint;
     public TetrisBlock Prefab;
-    // Start is called before the first frame update
+
+    public Transform[,] Grid;
+
+    public bool IsCellAvailable(int row, int col) => Grid[row, col] == null;
+
+    readonly int Rows = 20;
+    readonly int Cols = 10;
+
+    void Awake()
+    {
+        Grid = new Transform[Rows, Cols];
+    }
+
     void Start()
     {
-        GameObject.Instantiate(Prefab, SpawnPoint.position, Quaternion.identity);
-        
+        InstantiateTetrisBlock();
     }
 
     public void InstantiateTetrisBlock()
     {
-        GameObject.Instantiate(Prefab, SpawnPoint.position, Quaternion.identity);
+        var tetrisBlock = GameObject.Instantiate(Prefab,
+                                                 SpawnPoint.position,
+                                                 Quaternion.identity);
+        tetrisBlock.Board = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddBlockToGrid(TetrisBlock tetrisBlock)
     {
-        
+        var tetrisBlockTrans = tetrisBlock.transform;
+        for (int i = 0; i < tetrisBlockTrans.childCount; i++)
+        {
+            var childTrans = tetrisBlockTrans.GetChild(i);
+            int x = Mathf.RoundToInt(childTrans.position.x);
+            int y = Mathf.RoundToInt(childTrans.position.y);
+            var row = y;
+            var col = x;
+            Grid[row, col] = childTrans;
+        }
+    }
+
+    public void RemoveBlockFromGrid(TetrisBlock tetrisBlock)
+    {
+        var tetrisBlockTrans = tetrisBlock.transform;
+        for (int i = 0; i < tetrisBlockTrans.childCount; i++)
+        {
+            var childTrans = tetrisBlockTrans.GetChild(i);
+            int x = Mathf.RoundToInt(childTrans.position.x);
+            int y = Mathf.RoundToInt(childTrans.position.y);
+            var row = y;
+            var col = x;
+            Grid[row, col] = null;
+        }
+    }
+
+    public void ApplyClearanceRule()
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            var filled = true;
+            for (int j = 0; j < Cols; j++)
+            {
+                if (Grid[i, j] == null)
+                {
+                    filled = false;
+                    break;
+                }
+            }
+            if (filled)
+            {
+                var removed = new TetrisBlock[Cols];
+                for (int j = 0; j < Cols; j++)
+                {
+                    var child = Grid[i, j];
+                    var tetrisBlock = child.GetComponentInParent<TetrisBlock>();
+                    GameObject.Destroy(child.gameObject);
+                    Grid[i, j] = null;
+                    removed[j] = tetrisBlock;
+                }
+                for (int k = 0; k < Cols; k++)
+                {
+                    var tetrisBlock = removed[k];
+                    RemoveBlockFromGrid(tetrisBlock);
+                    tetrisBlock.enabled = true;
+                }
+            }
+        }
     }
 }
