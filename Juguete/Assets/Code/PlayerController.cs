8f2ae8e8b55273c464bb3bb0c9a85adee15a290f
Diego <dimamuno@espol.edu.ec>
2019-11-18 14:34:58 -0500

    Movement bugs fixed

--- Juguete/Assets/Code/PlayerController.cs
+++ Juguete/Assets/Code/PlayerController.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public Rigidbody2D rb;
-    public Animator anim;
+    /*[SerializeField] to see the variable in unity*/ private Rigidbody2D rb;
+    /*[SerializeField]*/ private Animator anim;
     // Start is called before the first frame updateasas
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        float hdirection = Input.GetAxis("Horizontal");
+
+        if (hdirection < 0)
         {
             rb.velocity = new Vector2(-3, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
             anim.SetBool("running", true);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (hdirection>0)
         {
             rb.velocity = new Vector2(3, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
@@ -31,6 +34,7 @@ public class PlayerController : MonoBehaviour
         else
         {
             anim.SetBool("running", false);
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
         if(Input.GetKeyDown(KeyCode.W))
         {
