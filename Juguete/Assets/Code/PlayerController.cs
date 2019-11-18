9ad11151227882184b53ef4160609d5427ee0301
Diego <dimamuno@espol.edu.ec>
2019-11-18 09:04:12 -0500

    Player 1 movement have been added

---
+++ Juguete/Assets/Code/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-3,rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(3, rb.velocity.y);
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 7f);
+        }
+    }
+}
